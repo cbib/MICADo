40d8641a0f55a0f4991e8aa7a97742d03dff9c1b
JRudewicz <justinerudewicz@hotmail.fr>
2015-07-20 17:13:00 +0200

    Create principal.py

---
+++ src/principal.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding=utf8
+## imports
+
+import networkx as nx
+from argparse import ArgumentParser
+from helpers.helpers import time_iterator, get_or_create_dir
+from helpers.logger import init_logger
+import sys
